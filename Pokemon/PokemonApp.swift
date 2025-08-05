c8493a5f733652d260ec88a82d836a281a1a5032
solohubivan <solohubivan@gmail.com>
2025-08-05 17:05:59 +0300

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Ivan Solohub on 05.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
